7a630319fb63c29b8b43b5710cfc944c98fa19ba
Kbjareanrak <bins_aptx@hotmail.com>
2019-09-22 14:26:55 +0700

    Commit Final

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
     <div className="posText">
-      <h2 style={{ color:'rgb(10, 51, 26)'}}>Wellcom to Game Card </h2>
+      <h2 style={{ color:'rgb(10, 51, 26)'}}>Wellcom to Game Card! </h2>
       <WordCard value="hello"/>
       <h3>Kanokwan Jareanrak 5935512090</h3>
     </div>
